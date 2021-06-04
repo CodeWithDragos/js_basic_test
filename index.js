b51adb0fada9bb53a9f76cea66d1795b55da82dc
Bogdan Nedelcu <bogdanned32@gmail.com>
2021-06-04 13:44:47 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function sum(a, b){
+    if(typeof a !== "number") return 0
+    return a + b
+}
+
+
+module.exports = sum;
+
---
+++ index.spec.ts
@@ -0,0 +1,14 @@
+const sum = require("./index")
+
+
+describe("Sums two numbers", () =>{
+    test("It can sum 3 and 3 and returns 6", () => {
+        const result = sum(3, 3)
+        expect(result).toEqual(6)
+    })
+    // unhappy path
+    test("If a is a string should return null", () => {
+        const result = sum("3", 3)
+        expect(result).toEqual(null)
+    })
+} )
